7828caf2f1f775b362e7af8d788515117855cfbe
Iván Pérez <ivan_psh@hotmail.com>
2026-02-19 09:29:40 +0100

    Angular first project 'My Store' completed

---
+++ README.md
@@ -0,0 +1,18 @@
+# Angular_EN
+
+Repository dedicated to learning projects based on the official documentation of [Angular](https://angular.dev) and (https://v17.angular.io).
+
+This repository contains multiple practical projects developed while studying and experimenting with the Angular framework.
+
+---
+
+## 📚 Purpose
+
+The goal of this repository is to:
+
+- Learn and reinforce fundamental Angular concepts.
+- Practice best practices recommended in the official documentation.
+- Understand the architecture and internal workings of the framework.
+- Serve as a future reference with functional examples.
+
+---
--- nw4rmebn/src/app/cart/cart.component.ts
+++ nw4rmebn/src/app/cart/cart.component.ts
@@ -2,18 +2,32 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CartService } from '../cart.service';
 import { RouterModule } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
   items = this.cartService.getItems()
 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  })
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ){}
+
+  onSubmit(): void {
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value)
+    this.checkoutForm.reset()
+  }
 }
